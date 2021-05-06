71c30a7f9e8487bb0211222fcb8ff013f5e87320
Jared Tauler <81237109+JaredTauler@users.noreply.github.com>
2021-05-06 12:49:16 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import flask
-from flask import Flask, render_template, request, escape
+from flask import Flask, render_template, request, redirect, url_for
+# import flask-user
 from sqlalchemy import create_engine
 import pandas as pd
 
@@ -16,9 +17,13 @@ def Login():
         # getting input with name = lname in HTML form
         last_name = request.form.get("lname")
         print(first_name)
-        return "Your name is " + first_name + last_name
+        return redirect(url_for("Home"))
     return render_template("test.html")
     # return render_template("test.html", name={escape(name)})
+
+@app.route("/home")
+def Home():
+    return "home"
 
 @app.route("/<a>")
 def printer(a):
