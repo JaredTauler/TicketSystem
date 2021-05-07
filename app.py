cd104dd28f7051c5e2718579f0d35138b8d5a166
Jared Tauler <81237109+JaredTauler@users.noreply.github.com>
2021-05-07 07:04:35 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+import flask
+from flask import Flask, render_template, request, redirect, url_for
+# import flask-user
+from sqlalchemy import create_engine
+import pandas as pd
+
+# DB = create_engine("sqlite:///1.db")
+# a = pd.read_sql_table("item", con=DB)
+
+app = Flask(__name__)
+
+@app.route('/', methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        # getting input with name = fname in HTML form
+        first_name = request.form.get("fname")
+        # getting input with name = lname in HTML form
+        last_name = request.form.get("lname")
+        print(first_name)
+        return redirect(url_for("home"))
+    return render_template("login.html")
+    # return render_template("login.html", name={escape(name)})
+
+@app.route("/home")
+def home():
+    return "home"
+
+@app.route("/<a>")
+def printer(a):
+    return a
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
