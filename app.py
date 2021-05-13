32f31eed95bacd034e40b8ce846a07eebd25f730
Jared Tauler <81237109+JaredTauler@users.noreply.github.com>
2021-05-13 12:47:31 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -5,9 +5,9 @@
 # pip install mysqlclient, redis, cryptography
 # or you will crash.
 
-from flask import Flask, render_template, request, redirect, url_for, session,
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_session.__init__ import Session
-from flask.socketio import SocketIO, emit
+import flask_socketio as io
 # from flask_sqlalchemy import SQLalchemy
 
 import pandas as pd
@@ -49,6 +49,7 @@ class DatabaseError(TicketSystemError): pass
 ## Main program
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "211910"
+socketio = io.SocketIO(app, cors_allowed_origins="*")
 # print(ConnectionString("jlt", "1234", "localhost"))
 
 # Connect to DB server
@@ -75,33 +76,38 @@ engine = create_engine(ConnectionString("jlt", "1234", "localhost"))  # pip inst
 def Login():
     print(request.method)
     session.clear()
-    if request.method == "POST":
-        if request.form.get('login'):
-            username = request.form.get("username")
-            password = request.form.get("password")
-            form = {}
-            for i in ["password", "username"]:
-                form[i] = request.form.get(i)
-
-            user = pd.read_sql_query(f"SELECT * FROM `ticketsystem`.`user` WHERE `username` LIKE '{form['username']}'", engine)
-            if user.empty:
-                raise DatabaseError("User not in Database")
-
-            password = bytes.fromhex(user["password"].values[0])
-            salt = bytes.fromhex(user["salt"].values[0])
-
-            hashed = hashlib.pbkdf2_hmac('sha256',form["password"].encode('utf-8'), salt, 100000)
-
-            if hashed != password:
-                return "Bad password!"
-
-            session["user"] = user["username"].values[0]
-            session["email"] = user["email"].values[0]
-            print(session)
-            return redirect(url_for("Home"))
-        elif request.form.get('signup'): return redirect(url_for("Signup"))
-
     return render_template("login.html")
+
+@socketio.event
+def LoginIO(data):
+    print(data)
+
+    # if request.method == "POST":
+    #     if request.form.get('login'):
+    #         username = request.form.get("username")
+    #         password = request.form.get("password")
+    #         form = {}
+    #         for i in ["password", "username"]:
+    #             form[i] = request.form.get(i)
+    #
+    #         user = pd.read_sql_query(f"SELECT * FROM `ticketsystem`.`user` WHERE `username` LIKE '{form['username']}'", engine)
+    #         if user.empty:
+    #             raise DatabaseError("User not in Database")
+    #
+    #         password = bytes.fromhex(user["password"].values[0])
+    #         salt = bytes.fromhex(user["salt"].values[0])
+    #
+    #         hashed = hashlib.pbkdf2_hmac('sha256',form["password"].encode('utf-8'), salt, 100000)
+    #
+    #         if hashed != password:
+    #             return "Bad password!"
+    #
+    #         session["user"] = user["username"].values[0]
+    #         session["email"] = user["email"].values[0]
+    #         print(session)
+    #         return redirect(url_for("Home"))
+    #     elif request.form.get('signup'): return redirect(url_for("Signup"))
+
 
 
 @app.route("/")
@@ -113,47 +119,46 @@ def Home():
 
 
 @app.route("/signup", methods=["GET", "POST"])
-def Signup():
-    if request.method == "POST":
+def Signup(): return render_template("signup.html")
 
-        form = {}
-        for i in ["password", "confirmpassword", "username", "email"]:
-            form[i] = request.form.get(i)
-        try:
-            # TODO Do all checks that don't require connecting to the database in the HTML with scripts.
-            for i in ["password", "username", "email"]:
-                # Check for blank or only space characters
-                if form[i].isspace() or form[i] == "":
-                    raise BadInputError(f"Your {i.capitalize()} cannot be blank.")
-                # Check for space characters
-                if form[i].find(" ") != -1:
-                    raise BadInputError(f"Your {i.capitalize()} cannot have spaces.")
+# Script for sign up
+@socketio.event
+def SignupSubmit(form):
+    problem = []
+    if form["password"] != form["confirmpassword"]:
+        problem.append("Passwords do not match.")
 
-            # Check password
-            if form["password"] != form["confirmpassword"]:
-                raise BadInputError("Passwords do not match.")
+    for i in ["password", "username", "email"]:
+        # Check for blank or only space characters
+        if form[i].isspace() or form[i] == "":
+            problem.append(f"Your {i.capitalize()} cannot be blank.")
+        # Check for space characters
+        if form[i].find(" ") != -1:
+            problem.append(f"Your {i.capitalize()} cannot have spaces.")
 
-            # Check if username is taken. SQL command will return rows with matching username. (which will never be more than 1)
-            if len(ExecuteDB(f"SELECT * FROM `ticketsystem`.`user` WHERE `username` LIKE '{form['username']}'", engine, True)) > 0:
-                raise DatabaseError(f"The username {form['username']} is already taken.")
+    # Check if user is already in database or not. Length will be more than 0 if user is already present.
+    if len(
+        ExecuteDB(f"SELECT * FROM `ticketsystem`.`user` WHERE `username` LIKE '{form['username']}'", engine, True)
+    ) > 0:
+        problem.append(f"The username {form['username']} is already taken.")
 
-            salt = os.urandom(32)  # Remember this
-            hashed = hashlib.pbkdf2_hmac('sha256', form["password"].encode('utf-8'), salt, 100000)
+    if problem != []: io.emit('problem', problem)
+    else:
+        # password hashing
+        salt = os.urandom(32)
+        hashed = hashlib.pbkdf2_hmac('sha256', form["password"].encode('utf-8'), salt, 100000)
 
-            # Store hashed and salt as hex in the database.
-            ExecuteDB(
-                f"INSERT INTO `ticketsystem`.`user` (`username`, `email`, `password`, `salt`) VALUES \
-                ('{form['username']}', '{form['email']}', '{hashed.hex()}', '{salt.hex()}')"
+        # Store hashed and salt as hex in the database.
+        ExecuteDB(
+            f"INSERT INTO `ticketsystem`.`user` (`username`, `email`, `password`, `salt`) VALUES \
+            ('{form['username']}', '{form['email']}', '{hashed.hex()}', '{salt.hex()}')"
             , engine)
 
-            return redirect(url_for("Login"))
+        # redirect after succesfully signing up
+        io.emit('redirect', url_for("Login"))
 
-        except Exception as E:
-            return render_template("signup.html", error=E)
-
-
-    return render_template("signup.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    socketio.run(app, debug=True)
 
